cd8cc5adb337ed6e67815361a4f8ae1306e5ed8c
Alistair Broomhead <alistair.broomhead@gmail.com>
2014-09-04 20:37:08 +0100

    asd

--- hacker_typer/__init__.py
+++ hacker_typer/__init__.py
@@ -1,4 +1,4 @@
-import sys, termios, tty
+import sys, termios, tty, random, time
 
 template = """
 # python
@@ -19,23 +19,47 @@ template = """
 """
 
 def get_char():
+    ch = sys.stdin.read(1)
+    #print(repr(ch))
+    if ch == '\04':
+        raise EOFError()
+    if ch == '\x1b':
+        clear_screen()
+    return ch
+
+def clear_screen():
+    print('\n'*1000)
+    
+def main():
+    clear_screen()
+    i = 0
+    OKGREEN = '\033[92m'
+    OKBLUE = '\033[94m'
+    colors = ['\033[9%dm' % i for i in range(10)]
+    while True:
+        sz = random.randint(3, 7)
+        get_char()
+        outstr = template[i:i+sz]
+        for c in outstr:
+            if c == " ":
+                print(random.choice(colors),end='',file=sys.stderr)
+            print(c, end='')
+            sys.stdout.flush()
+            time.sleep(0.1)
+        i = i + sz
+        if i > len(template)-(sz-1):
+            i = 0
+        if not random.randint(0, 400):
+            clear_screen()
+
+
+
+if __name__ == '__main__':
     file_descriptor = sys.stdin.fileno()
 
     old_settings = termios.tcgetattr(file_descriptor)
     try:
         tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
+        main()
     finally:
         termios.tcsetattr(file_descriptor, termios.TCSADRAIN, old_settings)
-    return ch
-
-
-if __name__ == '__main__':
-    i = 0
-    while True:
-        get_char()
-        print(template[i:i+3], end='', file=sys.stderr)
-        sys.stderr.flush()
-        i = i + 3
-        if i > len(template)-2:
-            i = 0
